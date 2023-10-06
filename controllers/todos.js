076c6a6f6df6a824941c4a01960c0f5fa9979d66
caiomelotec <caio.melotec@icloud.com>
2023-10-06 20:15:16 +0200

    postDeleteTodo and EditTodo controllers

--- controllers/todos.js
+++ controllers/todos.js
@@ -13,7 +13,34 @@ exports.getTodos = (req, res) => {
 exports.postWriteTodos = (req, res) => {
   const todoTitle = req.body.todoName;
   const date = req.body.dateInput;
-  const todo = new Todos(todoTitle, date);
+  const todo = new Todos(todoTitle, date.replace("T", " Time: "));
   todo.writeTodos();
   res.redirect("/");
+};
+
+exports.postDeleteTodo = (req, res) => {
+  const todoId = req.body.id;
+  Todos.deleteTodoById(todoId);
+  res.redirect("/");
+};
+
+exports.editTodo = (req, res) => {
+  const todoId = req.params.id;
+  const updatedTitle = req.body.todoName;
+  const updatedDate = req.body.dateInput;
+
+  Todos.updateTodo(
+    todoId,
+    updatedTitle,
+    updatedDate.replace("T", " Time: "),
+    (err) => {
+      if (err) {
+        // Handle the error, return a 500 internal server error
+        res.status(500).json({ err: "Failed to edit todo." });
+      } else {
+        //if success redirect to home
+        res.redirect("/");
+      }
+    }
+  );
 };
